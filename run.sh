cef4bb6c022bdc2f422d5651ad9895478c02ec70
Juanma Hidalgo <juanma06@gmail.com>
2023-07-07 20:14:04 +0200

    feat: update scripts to run the substreams

--- run.sh
+++ run.sh
@@ -1,14 +1,3 @@
-# #!/bin/bash
-
-# # Run the first process (ethereum-mainnet) as a daemon
-# nohup ./substreams-sink-postgres run "psql://dev-node:insecure-change-me-in-prod@localhost:5432/substreams-example?sslmode=disable" "mainnet.eth.streamingfast.io:443" "./docs/tutorial/substreams.yaml" db_out --metrics-listen-addr=0.0.0.0:9102 2> logs-ethereum-mainnet.txt > /dev/null &
-
-# # Run the second process (ethereum-goerli) as a daemon
-# nohup ./substreams-sink-postgres run "psql://dev-node:insecure-change-me-in-prod@localhost:5432/substreams-example?sslmode=disable" "goerli.eth.streamingfast.io:443" "./docs/tutorial/substreams.yaml" db_out --metrics-listen-addr=0.0.0.0:9103 2> logs-ethereum-goerli.txt > /dev/null &
-
-# # Run the third process (polygon-mainnet) as a daemon
-# nohup ./substreams-sink-postgres run "psql://dev-node:insecure-change-me-in-prod@localhost:5432/substreams-example?sslmode=disable" "polygon.streamingfast.io:443" "./docs/tutorial/substreams.yaml" db_out --metrics-listen-addr=0.0.0.0:9104 2> logs-polygon-mainnet.txt > /dev/null &
-
 
 #!/bin/bash
 
@@ -21,11 +10,6 @@ run_process() {
     # Run the command in the background, save the PID to the file, and redirect the logs to the specified file
     ($command) > $log_file 2>&1 & echo $! > $pid_file
 }
-
-# Step 1: Download Go (if not already installed)
-
-# Step 2: Download the CLI using `go install`
-# go install github.com/streamingfast/substreams-sink-postgres/cmd/substreams-sink-postgres@latest
 
 # Prompt the user to enter the network
 read -p "Enter the network: (goerli, mainnet or polygon) " network
@@ -40,7 +24,7 @@ else
 fi
 
 # Run the process for the specified network
-run_process "./substreams-sink-postgres run $psql_string&schema=$psql_schema $network.streamingfast.io:443 substreams-$network.yaml $db_out --metrics-listen-addr=0.0.0.0:9102" process-$network.pid logs-$network.txt &
+run_process "./substreams-sink-postgres run $psql_string&schema=$psql_schema $network.streamingfast.io:443 substreams-$network.yaml $db_out --metrics-listen-addr=0.0.0.0:9102" $network-sink-pid.txt logs-$network.txt &
 
 # Print the PID file for reference
-echo "Process PID file: process.pid"
+echo "Process PID file: $network-sink-pid.txt"
---
+++ scripts/index.ts
@@ -0,0 +1,148 @@
+import SQL from "sql-template-strings";
+import { Pool, PoolClient } from "pg";
+import { Command } from "commander";
+import { exec } from "child_process";
+
+const SCHEMA_PREFIX = "dcl";
+const INITIAL_SCHEMA = `${SCHEMA_PREFIX}1`;
+
+async function getLatestSchema(client: PoolClient): Promise<string | null> {
+  try {
+    const query = `
+      SELECT information.schema_name
+      FROM information_schema.schemata AS information
+      WHERE schema_name LIKE $1::text || '%'
+      ORDER BY CAST(SUBSTRING(information.schema_name FROM 'dcl([0-9]+)') AS INTEGER) DESC
+      LIMIT 1;
+    `;
+
+    const result = await client.query(query, [SCHEMA_PREFIX]);
+
+    if (result.rowCount > 0) {
+      const latestSchema = result.rows[0].schema_name;
+      return latestSchema;
+    } else {
+      return null; // No matching schema found
+    }
+  } catch (error) {
+    console.error("Error retrieving latest schema:", error);
+    return null;
+  }
+}
+
+function incrementSchema(schema: string) {
+  // Extract the number from the input string
+  const schemaNumber = schema.match(/\d+/)?.[0];
+  if (schemaNumber) {
+    const number = parseInt(schemaNumber);
+    const incrementedNumber = number + 1;
+    // Replace the number in the string with the incremented value
+    return schema.replace(/\d+$/, `${incrementedNumber}`);
+  }
+  throw new Error("Invalid schema");
+}
+
+export async function createNewSchema(client: PoolClient, network: string) {
+  const latestSchema = await getLatestSchema(client);
+  const newSchema = incrementSchema(latestSchema || INITIAL_SCHEMA);
+  console.log("latestSchema: ", latestSchema);
+  console.log("newSchema: ", newSchema);
+  try {
+    await client.query(SQL`CREATE SCHEMA IF NOT EXISTS `.append(newSchema));
+    await client.query(
+      `INSERT INTO substreams.deployments (schema, network) VALUES ($1, $2)`,
+      [newSchema, network]
+    );
+    const createCursorQuery = SQL`
+        CREATE TABLE `
+      .append(newSchema)
+      .append(
+        SQL`.cursors (
+          id text PRIMARY KEY,
+          cursor text,
+          block_num bigint,
+          block_id text
+      );
+      
+      -- Indices -------------------------------------------------------
+      
+      CREATE UNIQUE INDEX cursor_pk ON `
+          .append(newSchema)
+          .append(SQL`.cursors(id text_ops)`)
+      );
+    await client.query(createCursorQuery);
+  } catch (error) {
+    console.error(error);
+    throw Error("Error creating new schema");
+  }
+  return newSchema;
+}
+
+// Function to run the sink command
+function runSinkSetupCommand(
+  db: string,
+  callback: (error: Error | null, stdout: string, stderr: string) => void
+) {
+  exec(
+    `substreams-sink-postgres setup '${db}' ../schema.sql`,
+    (error, stdout, stderr) => {
+      callback(error, stdout, stderr);
+    }
+  );
+}
+
+async function main() {
+  // Define command-line options
+  const program = new Command();
+  program.option("-d, --db <connectionString>", "Database connection string");
+  program.option(
+    "-n, --network <network>",
+    "Network that the schema belongs to"
+  );
+
+  // Parse the command-line arguments
+  program.parse(process.argv);
+  const options = program.opts();
+
+  // Retrieve the database connection string from the command-line options
+  const dbConnectionString = options.db;
+  if (!dbConnectionString) {
+    throw new Error(
+      "Database connection string is required. E.g: --db postgres://user:password@localhost:5432/db_name"
+    );
+  }
+  // Retrieve the network string from the command-line options
+  const network = options.network;
+  console.log("network: ", network);
+  if (!network) {
+    throw new Error("Network is required. E.g: --network polygon");
+  }
+  console.log("dbConnectionString: ", dbConnectionString);
+
+  const pool = new Pool({ connectionString: dbConnectionString });
+  const client = await pool.connect();
+
+  //   const latestSchema = await getLatestSchema(client);
+
+  await createNewSchema(client, network);
+
+  runSinkSetupCommand(
+    dbConnectionString,
+    (error: Error | null, stdout: string, stderr: string) => {
+      if (error) {
+        console.error("Error running sink command:", error);
+      }
+
+      console.log("Sink command executed successfully:", stdout);
+    }
+  );
+
+  client.release();
+  // Close the database connection pool
+  await pool.end();
+}
+
+main().catch((error) => {
+  console.error("Script execution error:", error);
+  process.exit(1);
+});

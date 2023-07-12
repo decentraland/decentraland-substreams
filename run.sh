4be8b8f445e6618cc6181f2f639c32005ec2cd5f
Juanma Hidalgo <juanma06@gmail.com>
2023-07-12 13:22:13 +0200

    feat: update run script to compute prometheus metrics port

--- run.sh
+++ run.sh
@@ -17,6 +17,23 @@ read -p "Enter the PostgreSQL connection string: " psql_string
 read -p "Enter the PostgreSQL schema to sink to: " psql_schema
 read -p "Enter the spkg url or yaml: " spkg_string
 
+# Set the prometheus_port based on the network
+case $network in
+    "polygon")
+        prometheus_port="9102"
+        ;;
+    "mainnet")
+        prometheus_port="9103"
+        ;;
+    "goerli")
+        prometheus_port="9104"
+        ;;
+    *)
+        echo "Invalid network provided!"
+        exit 1
+        ;;
+esac
+
 # Determine the db_out parameter based on the network
 if [ "$network" = "polygon" ]; then
     db_out="db_out_polygon"
@@ -25,7 +42,10 @@ else
 fi
 
 # Run the process for the specified network
-run_process "./substreams-sink-postgres run $psql_string&schema=$psql_schema $network.streamingfast.io:443 $spkg_string $db_out --metrics-listen-addr=0.0.0.0:9102" $network-sink-pid.txt logs-$network.txt &
+run_process "./substreams-sink-postgres run $psql_string&schema=$psql_schema $network.streamingfast.io:443 $spkg_string $db_out --metrics-listen-addr=0.0.0.0:$prometheus_port" $network-sink-pid.txt logs-$network.txt &
+
+# Print the Prometheus port for reference
+echo "Prometheus metrics exported on port: $prometheus_port"
 
 # Print the PID file for reference
 echo "Process PID file: $network-sink-pid.txt"

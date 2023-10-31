ad21cde6b04eeacf9c2790b63d6f2e2b18f72414
Juanma Hidalgo <juanma06@gmail.com>
2023-10-31 14:59:38 +0100

    feat: add refresh_materialzed_views script

---
+++ scripts/refresh_materialized_views.js
@@ -0,0 +1,53 @@
+// run in background and dump logs to file. E.g: node refresh_materialized_view.js > refresh_views_logs.txt 2>&1 &
+const { Client } = require("pg");
+const parse = require("pg-connection-string").parse;
+
+const connectionString = process.env.SUBSTREAMS_DATABASE;
+
+if (!connectionString) {
+  console.error("Please set the SUBSTREAMS_DATABASE environment variable");
+  process.exit(1);
+}
+
+const config = parse(connectionString);
+
+if (!config) {
+  console.error("Error parsing connection string");
+  process.exit(1);
+} else {
+  const client = new Client(config);
+
+  client.connect();
+
+  client.query("LISTEN refresh_mat_view");
+
+  client.on("notification", (msg) => {
+    if (msg.channel === "refresh_mat_view") {
+      const materializedView = msg.payload;
+      console.log('materializedView: ', materializedView);
+
+      client
+        .query(`REFRESH MATERIALIZED VIEW CONCURRENTLY ${materializedView}`)
+        .then(() =>
+          console.log(
+            `Materialized view ${materializedView} refreshed successfully`
+          )
+        )
+        .catch((err) =>
+          console.error(
+            `Error refreshing materialized view ${materializedView}:`,
+            err
+          )
+        );
+    }
+  });
+
+  client.on("error", (err) => {
+    console.error("Error with PostgreSQL client:", err);
+  });
+
+  process.on("SIGINT", () => {
+    client.end();
+    process.exit();
+  });
+}
